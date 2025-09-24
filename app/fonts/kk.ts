b76905f7723a9ff68ab0dfdcd894a5ddee185ac0
kaarlekaarle <kaarle.hurtig@gmail.com>
2025-09-24 13:37:13 +0300

    Fix font path for build compatibility

--- app/fonts/kk.ts
+++ app/fonts/kk.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const kk = localFont({
-  src: '/fonts/kk-regular.otf',
+  src: '../../public/fonts/kaarle-kumppanit/kk_regular.otf',
   variable: '--font-kk',
   display: 'swap',
 })
